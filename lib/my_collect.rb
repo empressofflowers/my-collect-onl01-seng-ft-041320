ea9336d8e001f166fd1e0bb541e044a507adceca
Devone Austin <devone.austin@gmail.com>
2020-04-18 02:31:05 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,9 +3,8 @@ require "pry"
 def my_collect(array)
   n = 0
   new_array = []
-  while n > array.size
+  while n < array.size
     new_array << yield(array[n])
-    binding.pry
     n = n + 1
   end
   new_array
